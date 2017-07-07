c53d79c1569f6672876220e84e3ced4fbc3cd88c
Lucas Roussel <lucas.roussel@ensiie.fr>
2017-07-07 10:29:33 +0200

    temporary solution

--- build/CMakeFiles/TRAJECTORY-MODEL.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/TRAJECTORY-MODEL"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/TRAJECTORY-MODEL.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
